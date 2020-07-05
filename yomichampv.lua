724ac60357a537a1a125d4c9f8d738f32fabd6d5
Lael N. Santos <lael@alunos.utfpr.edu.br>
2020-07-04 23:10:22 -0300

    yomichampv.lua: Added

---
+++ yomichampv.lua
@@ -0,0 +1,65 @@
+-- v. 0.1
+-- Push subtitles to clipboard
+-- in linux needs xsel to work
+--
+-- default keybinding to start/stop: Y
+
+-- TODO: make it portable
+local clipboard = 'xsel -z -b'
+local running = false
+
+local function toclipboard(name, value)
+	if running and type(value) == "string" then
+		io.popen(clipboard, 'w'):write(value):close()
+	end
+end
+
+local function stop()
+	running = false
+	mp.msg.warn('Quitting Yomichampv ...')
+	mp.unregister_event("end-file", stop)
+	mp.unobserve_property("sub-text", "string", toclipboard)
+	mp.remove_key_binding("Yomichampv-pause")
+	mp.remove_key_binding("Yomichampv-resume")
+end
+
+-- get_active_subtrack from huglovefan/mpv-subside.lua
+local function get_active_subtrack ()
+	local l = mp.get_property_native('track-list')
+	for _, t in ipairs(l) do
+		if t.type == 'sub' and t.selected then
+			return t.lang
+		end
+	end
+	return 'no'
+end
+
+local function start_stop()
+	if running == true then
+		stop()
+		mp.command('show-text "Quitting Yomichampv..."')
+	else
+		if get_active_subtrack() ~= 'ja' then
+			mp.command('show-text "Select japanese subtitles before' ..
+				' starting Yomichampv."')
+		else
+			running = true
+			mp.msg.warn('Starting Yomichampv ...')
+			mp.register_event("end-file", stop)
+			mp.observe_property("sub-text", "string", toclipboard)
+			mp.add_forced_key_binding("mouse_leave", "Yomichampv-pause",
+				function () mp.set_property_native("pause", true) end)
+			mp.add_forced_key_binding("mouse_move", "Yomichampv-resume",
+				function ()
+					if mp.get_property_native("pause") then
+						mp.set_property_native("pause", false)
+					end
+				end
+			)
+
+			mp.command('show-text "Starting Yomichampv..."')
+		end
+	end
+end
+
+mp.add_key_binding("y", "start-stop-Yomichampv", start_stop)
