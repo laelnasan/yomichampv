1b1f3088c76fccc6b7bbeebd3ec238b931980151
laelnasan <laelnasan@users.noreply.github.com>
2020-11-13 19:08:17 -0300

    cpu usage mouse_move binding is negligible

--- yomichampv.lua
+++ yomichampv.lua
@@ -1,25 +1,19 @@
--- v. 0.1
+-- v. 0.2
 -- Push subtitles to clipboard
--- in linux needs xsel to work
+-- in linux needs xsel or xclip to work
 --
 -- default keybinding to start/stop: Y
 
--- verify which command to use
-local clipboard
-if package.config:sub(1,1) == '\\' then -- Windows
-	clipboard = 'clip.exe'
-elseif os.execute('xsel -h > /dev/null 2>&1') then
-	clipboard = 'xsel -z -b'
-elseif os.execute('xclip -h > /dev/null 2>&1') then
-	clipboard = 'xclip -i -selection clipboard'
-elseif pcall(function () io.popen('pbcopy'):close() end) then
-	clipboard = 'pbcopy'
-end
+-- verify which command to use (false if none is found)
+local clipboard = package.config:sub(1,1) == '\\' and 'clip.exe' or                 -- Windows
+	(pcall(function () io.popen('pbcopy'):close() end)) and 'pbcopy' or         -- MacOS
+	os.execute('xsel -h > /dev/null 2>&1') and 'xsel -z -b' or                  -- xsel
+	os.execute('xclip -h > /dev/null 2>&1') and 'xclip -i -selection clipboard' -- xclip
 
 local running
 
 local function toclipboard(name, value)
-	if running and type(value) == "string" then
+	if running and type(value) == 'string' then
 		io.popen(clipboard, 'w'):write(value):close()
 	end
 end
@@ -27,10 +21,10 @@ end
 local function stop()
 	running = false
 	mp.msg.warn('Quitting Yomichampv ...')
-	mp.unregister_event("end-file", stop)
-	mp.unobserve_property("sub-text", "string", toclipboard)
-	mp.remove_key_binding("Yomichampv-pause")
-	mp.remove_key_binding("Yomichampv-resume")
+	mp.unregister_event('end-file', stop)
+	mp.unobserve_property('sub-text', 'string', toclipboard)
+	mp.remove_key_binding('Yomichampv-pause')
+	mp.remove_key_binding('Yomichampv-resume')
 end
 
 -- get_active_subtrack from huglovefan/mpv-subside.lua
@@ -54,22 +48,19 @@ local function start_stop()
 				' starting Yomichampv."')
 		else
 			running = true
-			mp.msg.warn('Starting Yomichampv ...')
-			mp.register_event("end-file", stop)
-			mp.observe_property("sub-text", "string", toclipboard)
-			mp.add_forced_key_binding("mouse_leave", "Yomichampv-pause",
-				function () mp.set_property_native("pause", true) end)
-			mp.add_forced_key_binding("mouse_move", "Yomichampv-resume",
-				function ()
-					if mp.get_property_native("pause") then
-						mp.set_property_native("pause", false)
-					end
-				end
-			)
+
+			mp.register_event('end-file', stop)
+			mp.observe_property('sub-text', 'string', toclipboard)
+
+			mp.add_forced_key_binding('mouse_leave', 'Yomichampv-pause',
+				function () mp.set_property_native('pause', true) end)
+
+			mp.add_forced_key_binding('mouse_move', 'Yomichampv-resume',
+				function () mp.set_property_native('pause', false) end)
 
 			mp.command('show-text "Starting Yomichampv..."')
 		end
 	end
 end
 
-mp.add_key_binding("y", "start-stop-Yomichampv", start_stop)
+mp.add_key_binding('y', 'start-stop-Yomichampv', start_stop)
